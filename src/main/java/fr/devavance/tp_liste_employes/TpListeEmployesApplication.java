ae601be703cd9511468b4e722db931bf451271f5
bl <bouchkhaf@gmail.com>
2024-02-01 17:23:55 +0100

    Version initiale

---
+++ src/main/java/fr/devavance/tp_liste_employes/TpListeEmployesApplication.java
@@ -0,0 +1,13 @@
+package fr.devavance.tp_liste_employes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpListeEmployesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpListeEmployesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/devavance/tp_liste_employes/TpListeEmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devavance.tp_liste_employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpListeEmployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
